3fc2cd3faa3d5ad06a7d271cab8469485d8ec7a8
IngElenaKozlova <bc.elena.kozlova@gmail.com>
2025-06-25 17:26:47 +0200

    v 0.00.002

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import express from 'express'
 const start = async () => {
     const port = +process.env.PORT || 3000;
     const host = process.env.HOST || '127.0.0.1'; 
+    const connectURL = `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@usersmongodb.7qfjiva.mongodb.net/`
     
     const app = express();
     app.use(express.json());
