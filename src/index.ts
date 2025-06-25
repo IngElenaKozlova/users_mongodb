ebc87b17f143edec442c4b9cae7a801e21b59617
IngElenaKozlova <bc.elena.kozlova@gmail.com>
2025-06-25 17:04:58 +0200

    v 0.00.001

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+require('dotenv').config()
+import express from 'express'
+// const usersRoute = require('./routes/users')
+
+
+const start = async () => {
+    const port = +process.env.PORT || 3000;
+    const host = process.env.HOST || '127.0.0.1'; 
+    
+    const app = express();
+    app.use(express.json());
+
+    // app.use('/users', usersRoute)
+
+    app.listen(port, host, () => {
+        console.log(`Server running at http://${host}:${port}`) 
+    })
+}
+
+
+start()
