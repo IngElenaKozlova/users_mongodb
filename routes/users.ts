93d851c4a71b6fed3d924c47dbf19955248132d1
IngElenaKozlova <bc.elena.kozlova@gmail.com>
2025-07-16 19:21:16 +0200

    v 0.00.004

--- routes/users.ts
+++ routes/users.ts
@@ -1,6 +1,7 @@
 const {Router} = require("express")
 const router = Router()
 const schemaUser = require('../scheme/user.scheme')
+import {getIsValidNumber} from '../src/validation'
 
 
 // import controlerShop from '../control/shop'
@@ -11,8 +12,7 @@ const schemaUser = require('../scheme/user.scheme')
 router.post('/createUser', async (req, res) => {
     try{   
         const userInstance = new schemaUser(req.body)
-
-        const response = await userInstance.save();
+        const response = await userInstance.save()
 
         // console.log(response)
             // .then(doc => {
@@ -32,15 +32,43 @@ router.post('/createUser', async (req, res) => {
 
 router.get('/getUser/:id', async (req, res) => {
     try{   
-        const userId = req.params.id;
+        const userId = req.params.id
         const user = await schemaUser.findById(userId)
-
         return res.status(200).json(user)
-
     } catch (e){
         return res.status(500).json(e.errorResponse)
     }
 })
+
+
+router.get('/getUsers', async (req, res) => {
+    try{   
+        const {page, count} = req.body
+
+        const pageValidation = getIsValidNumber(page)
+        if (!pageValidation) return res.status(409).json('page is not a number') 
+        const countValidation = getIsValidNumber(count)
+        if (!countValidation) return res.status(409).json('count is not a number')
+
+        const users = await schemaUser.find().skip((page-1)*count).limit(count)
+        return res.status(200).json(users)
+    } catch (e){
+        return res.status(500).json(e.errorResponse)
+    }
+})
+
+
+router.delete('/deleteUser/:id', async (req, res) => {
+    try{   
+        const userId = req.params.id
+        const deletedUser = await schemaUser.findByIdAndDelete(userId)
+        return res.status(200).json(deletedUser)
+    } catch (e){
+        return res.status(500).json(e.errorResponse)
+    }
+})
+
+
 
 
 module.exports = router
---
+++ src/validation.ts
@@ -0,0 +1,7 @@
+export const getIsValidNumber = (elem) => {
+    if (typeof elem === 'number' && elem > 0) return true
+    return false
+}
+
+
+
